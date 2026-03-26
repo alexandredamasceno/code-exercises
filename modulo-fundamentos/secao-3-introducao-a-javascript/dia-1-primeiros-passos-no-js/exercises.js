a02a6e81184eaef435cc9b2f4f05ea9c0d5123b7
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2026-03-25 22:16:05 -0300

    Cria um arquivo de exercicios para praticar estruturas de condições e lógica

---
+++ modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-primeiros-passos-no-js/exercises.js
@@ -0,0 +1,73 @@
+/* let reiInfo = "Move-se uma casa para qualquer direção (horizontal, vertical ou diagonal).";
+let damaInfo = "Move-se quantas casas quiser em qualquer direção (horizontal, vertical ou diagonal).";
+let peaoInfo = "Move-se uma casa à frente (duas no primeiro lance) e captura apenas na diagonal.";
+let bispoInfo = "Move-se quantas casas quiser apenas nas diagonais";
+let torreInfo = "Move-se quantas casas quiser em linhas retas (horizontal ou vertical)";
+let cavaloInfo = "Move-se em forma de 'L' (duas casas numa direção e uma para o lado) e pula peças.";
+
+let expressao = "macaco";
+
+switch (expressao.toLowerCase()) {
+    case "cavalo":
+        console.log(cavaloInfo);
+    break;
+    case "peao":
+        console.log(peaoInfo);
+    break;
+    case "rei":
+        console.log(reiInfo);
+    break;
+    case "dama":
+        console.log(damaInfo);
+    break;
+    case "bispo":
+        console.log(bispoInfo);
+    break;
+    case "torre":
+        console.log(torreInfo);
+    break;
+    default:
+        console.log("Informação inválida! Tente novamente.");
+} */
+
+
+
+/* Exercicio 4: mostre quando a soma dos três lados for um triângulo */
+/* let angulo1 = 90;
+let angulo2 = 30;
+let angulo3 = -50;
+
+let somaDosAngulos = angulo1 + angulo2 + angulo3;
+
+if (angulo1 > 0 && angulo2 > 0 && angulo3 > 0 && somaDosAngulos === 180) {
+    console.log(`${angulo1 + angulo2 + angulo3}; Parabéns, é um triângulo!`);
+} else if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0) {
+    console.log("Algum dos ângulos é inválido!!!");
+} else {
+    console.log("Não é um triângulo.");
+} */
+
+/* let number1 = 300;
+let number2 = 500;
+let number3 = 2500; */
+
+/* Exercicio 3: retorne o maior dentre três números */
+/* if (number1 > number2 && number1 > number3) {
+    console.log(`O número maior é: ${number1}`);
+} else if (number2 > number1 && number2 > number3) {
+    console.log(`O número maior é: ${number2}`);
+} else {
+    console.log(`O número maior é: ${number3}`);
+} */
+
+/* Exercicio 2: Retorna o número maior */
+/* if (number1 > number2) {
+    console.log(`O número maior é: ${number1}`);
+} else {
+    console.log(`O número maior é: ${number2}`);
+} */
+/* Exercicio 1 */
+/* console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 / number2);
+console.log(number1 % number2); */
