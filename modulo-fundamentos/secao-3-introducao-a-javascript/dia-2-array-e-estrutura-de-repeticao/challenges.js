1bf08891c4c1d54a5c9e92fe8164b6727c486c26
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2026-03-27 22:28:31 -0300

    Faz alguns desafios de lógica propostos pela Trybe

--- modulo-fundamentos/secao-3-introducao-a-javascript/dia-2-array-e-estrutura-de-repeticao/challenges.js
+++ modulo-fundamentos/secao-3-introducao-a-javascript/dia-2-array-e-estrutura-de-repeticao/challenges.js
@@ -1,6 +1,49 @@
+// Desafio da Piramide de asteriscos:
+const tamanhoDaPiramide = 7;
+let piramide = "";
+let piramideArray = "";
+for (let indexA = 0; indexA < tamanhoDaPiramide; indexA += 1) {
+    piramide += "*";
+    for (let indexB = 1; indexB < tamanhoDaPiramide; indexB += 1) {
+        piramide += "*";
+        if (piramide.split("").length == tamanhoDaPiramide) {
+            piramideArray += `${piramide}\n`;
+            piramide = "";
+        }
+    }
+}
+console.log(quadradoArray);
+
+
+//Desafio do QUADRADO DE asteriscos:
+/* const tamanhoDoQuadrado = 7;
+let quadrado = "";
+let quadradoArray = "";
+for (let indexA = 0; indexA < tamanhoDoQuadrado; indexA += 1) {
+    quadrado += "*";
+    for (let indexB = 1; indexB < tamanhoDoQuadrado; indexB += 1) {
+        quadrado += "*";
+        if (quadrado.split("").length == tamanhoDoQuadrado) {
+            quadradoArray += `${quadrado}\n`;
+            quadrado = "";
+        }
+    }
+}
+console.log(quadradoArray); */
+/* let nameA = "Alexandre";
+console.log(nameA.split("").length); */
+//Investe uma string
+/* let word = "dev";
+let charArray = word.split("");
+console.log(charArray);
+let charArrayReversed = charArray.reverse();
+console.log(charArrayReversed);
+let newWord = charArrayReversed.join("");
+console.log(newWord); */
+
 // Executa o fatorial de um número qualquer.
-let number  = 10;
-let fatorial = 1;
+/* let number  = 10;
+let fatorial = 1; */
 //Faz em ordem decrescente:
 /* for (let index = 10; index >= 1; index -= 1) {
     if (index != number) {
@@ -9,10 +52,10 @@ let fatorial = 1;
 }
 console.log(number); */
 // faz em ordem crescente
-for (let index = 1; index <= number; index += 1) {
+/* for (let index = 1; index <= number; index += 1) {
     fatorial *= index;
 }
-console.log(fatorial);
+console.log(fatorial); */
 
 //Busca a quantidade de números ímpares no array
 /* const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
