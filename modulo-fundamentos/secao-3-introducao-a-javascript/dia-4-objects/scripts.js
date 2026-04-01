a0be83b7aa99ec062cb0a73eb5f85da270393b7c
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2026-04-01 19:07:02 -0300

    Exercita métodos de Object, keys, value e entries e Constructor para criar objetos

--- modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-objects/scripts.js
+++ modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-objects/scripts.js
@@ -3,8 +3,34 @@ const player = {
     lastName: 'Silva',
     age: 34,
     medals: { golden: 2, silver: 3 },
-    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+    printHello: function() {
+        console.log(`Hello, world!!! e a idade é ${this.age}`); //com o this. eu consigo referenciar outra chave dentro de outra no mesmo object
+    }
 };
+//Método Object.keys() = traz todas as chaves de um objeto, somente as chaves e joga em um array de strings.
+const arrayKeys = Object.keys(player);
+//console.log(arrayKeys);
+//Método Object.values() = retorna um array com todos os valores daquele objeto mantendo seus respectivos tipos.
+const arrayValues = Object.values(player);
+//console.log(arrayValues);
+//Método Object,entries() = Cada propriedade(chave e valor) vira um array. É um array maior com vários arrays de propriedades dentro, mantendo seus respectivos tipos.
+const arrayArrays = Object.entries(player);
+//console.log(arrayArrays);
+//console.log(arrayArrays[4][1][0]);
+// Usando funções dentro do object
+//player.printHello();
+
+// CONSTUINDO UM OBJECT POR MEIO DE UMA FUNÇÃO = Object Constructor(Primeira letra Maior é boa prática). Deve-se usar o "new" na hora de criar um determinado objeto com o Contructor.
+function Person (name, age, course, year) {
+    this.name = name;
+    this.age = age;
+    this.course = course;
+    this.year = year;
+}
+const alexandreObject = new Person("Alexandre", 28, "ADS", 4);
+console.log(Object.entries(alexandreObject));
+
 // Notação de pontos ou Dote notation
 /* const message = `A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade`;
 console.log(message); */
@@ -30,13 +56,22 @@ const addKeysAndValue = (object, newKey, value) => {
      console.log(typeof object[newKey]);
     //object[newKey] = value;
 }
-addKeysAndValue(player, "birthPlace", 28);
+/* addKeysAndValue(player, "birthPlace", 28);
 addKeysAndValue(player, "birthPlace", 30);
 addKeysAndValue(player, "birthDate", "20/03/2000");
 addKeysAndValue(player, "birthDate", "20/03/2020");
 addKeysAndValue(player, "maritalStatus", "Casada");
 addKeysAndValue(player, "Hello", "World!!!");
-console.log(player);
+console.log(player); */
+console.log(typeof (typeof 20));
+/* console.log(typeof true);
+console.log(typeof true === "boolean"); */
+/* console.log(typeof player["birthDate"]);
+console.log(typeof player["age"]); */
+let myNumber;
+console.log(typeof myNumber == "undefined");
+//myNumber = 20;
+//console.log(typeof myNumber);
 
 
 
