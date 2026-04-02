f5294e14dd1ffbe7db67933f4cb08a8111fe7cfe
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2026-04-01 21:59:29 -0300

    Cria arquivos push.js para silumar um database e o arquivo pull.js para simular bem basicamente uma api. Exercicio bem básico para relembrar conceitos de export defaul e import... from...

---
+++ modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-objects/pull.js
@@ -0,0 +1,5 @@
+//const arrayPlayers = require("./push"); // formas de importar dados. Forma antiga
+import arrayPlayers from "./push.js"; // formas de importar dados
+console.log(typeof arrayPlayers);
+const players = arrayPlayers;
+console.log(players);
---
+++ modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-objects/push.js
@@ -0,0 +1,10 @@
+const players = {
+    name: "Alexandre",
+    age: 28,
+    club: "Barcelona",
+    city: "Madri",
+    country: "Brasil",
+};
+// formas de exportar dados
+export default players; // forma atual e uma boa prática
+//module.exports = players = forma antiga de fazer
