b2fd30e073d3f0d48ff3387e73fd8a929f07b035
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2026-04-02 20:13:32 -0300

    Faz três exercicios de manipulação de objetos. Utilizei um sistema de export e import para esse exercicos simulando um database e backend(API) bem simplificado

--- modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-objects/push.js
+++ modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-objects/push.js
@@ -1,10 +1,15 @@
-const players = {
-    name: "Alexandre",
-    age: 28,
-    club: "Barcelona",
-    city: "Madri",
-    country: "Brasil",
+const reader = {
+  name: 'Julia',
+  lastName: 'Pessoa',
+  age: 21,
+  favoriteBooks: [
+    {
+      title: 'O Senhor dos Anéis - a Sociedade do Anel',
+      author: 'J. R. R. Tolkien',
+      publisher: 'Martins Fontes',
+    },
+  ],
 };
 // formas de exportar dados
-export default players; // forma atual e uma boa prática
+export default reader; // forma atual e uma boa prática
 //module.exports = players = forma antiga de fazer
