8f5949451c323e032961cbbc190db19833c865dd
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2026-04-17 10:12:29 -0300

    Finaliza o desafio do leetcode, onde consistia em encontrar a menor distancia em um ARRAY CIRCULAR

--- modulo-fundamentos/secao-4-JS-DOM-eventos-e-webstorage/dia-2-js-trabalhando-com-elementos/leetcode-challeger.js
+++ modulo-fundamentos/secao-4-JS-DOM-eventos-e-webstorage/dia-2-js-trabalhando-com-elementos/leetcode-challeger.js
@@ -1,4 +1,5 @@
-const words = ["hsdqinnoha","mqhskgeqzr","zemkwvqrww","zemkwvqrww","daljcrktje","fghofclnwp","djwdworyka","cxfpybanhd","fghofclnwp","fghofclnwp"];
+const words = ["a", "b", "leetcode"];
+const wordsTwo = ["hsdqinnoha","mqhskgeqzr","zemkwvqrww","zemkwvqrww","daljcrktje","fghofclnwp","djwdworyka","cxfpybanhd","fghofclnwp","fghofclnwp"];
 
 const closestTarget = function(words, target, startIndex) {
     let arrayResults = [];
@@ -14,38 +15,58 @@ const closestTarget = function(words, target, startIndex) {
         }
     }
 
-
+    // Tenta nova lógica
     // inicia a logica do circular do startIndex até final do array
-    let countFromTheTop = 0;
-    let countFromTheTarget = 0;
-    // Ordem descrescente pegando do topo do array até o target
-    let wordsIndex = words.length - 1;
-    for (let index = wordsIndex; index >= 0 ; index -= 1) {
-        if (target === words[index]) {
-            countFromTheTop += 1;            
-            console.log(countFromTheTop);
-            break;
-        }
-        countFromTheTop += 1;
-    };
+    let countFromTheTopToTheStartIndex = 0;
+    let countFromTheBottomToTheTarget = 0;
 
-    // conta do indice a partir do target
-    for (let index = startIndex; index >= 0; index -= 1) {
-        if (index === 0) {
-            break;
+    let distanceFromTop = startIndex - (words.length - 1);
+    let distanceFromBottom =  startIndex - 0;
+    
+    if (Math.abs(distanceFromTop) < Math.abs(distanceFromBottom)) {
+        
+        let wordsIndex = words.length - 1;
+        for (let index = wordsIndex; index > startIndex; index -= 1) {
+            countFromTheTopToTheStartIndex += 1; 
         }
-        countFromTheTarget += 1;
+        //Agora pega do bottom até o target
+        for (let index = 0; index < words.length; index += 1) {
+            if (words[index] === target) {
+                countFromTheBottomToTheTarget += 1;
+                break;
+            }
+            countFromTheBottomToTheTarget += 1;
+        }
+        let sum = countFromTheTopToTheStartIndex + countFromTheBottomToTheTarget;
+        arrayResults.push(Math.abs(sum));
     }
-    // valor da distancia da circular
-    let countTotalLogicaCircular = countFromTheTop + countFromTheTarget;
-    arrayResults.push(countTotalLogicaCircular);
+
+    if (Math.abs(distanceFromTop) > Math.abs(distanceFromBottom)) {
+        let countFromStartIndexToTheBottom = 0;
+        let countFromTopToTheTarget = 0;
+        if (distanceFromBottom === 0) {
+            countFromStartIndexToTheBottom = 0;
+        }
+        for (let index  = startIndex - 1; index >= 0; index -= 1) {
+            countFromStartIndexToTheBottom += 1;
+        }
+        for (let index = words.length - 1; index >= 0; index -= 1) {
+            if (words[index] === target) {
+                countFromTopToTheTarget += 1;
+                break;
+            }
+            countFromTopToTheTarget += 1;
+        }
+        let sum = countFromStartIndexToTheBottom + countFromTopToTheTarget;
+        arrayResults.push(sum);
+    }
+
     let theBigger = arrayResults[0];
     for (let index = 0; index < arrayResults.length; index += 1) {
         if (theBigger > arrayResults[index]) {
             theBigger = arrayResults[index];
         }
     }
-    console.log(arrayResults);
     return theBigger;
 };
-console.log(closestTarget(words, "zemkwvqrww", 8));
+console.log(closestTarget(wordsTwo, "zemkwvqrww", 8));
